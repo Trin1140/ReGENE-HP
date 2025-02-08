ecaea83da8bbf78c0e232a9d0c6f3219da6f2dfd
Trin1140 <taichi1140@gmail.com>
2025-02-08 19:04:11 +0900

    画像とテキストの更新

--- next.config.js
+++ next.config.js
@@ -5,5 +5,8 @@ const withMDX = require('@next/mdx')({
   
   module.exports = withMDX({
     pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+    experimental: {
+      esmExternals: true,
+    },
   });
   
