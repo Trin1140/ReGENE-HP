a3f6482320e2c50160d31fa8cd34a0b8f0d81eec
Trin1140 <taichi1140@gmail.com>
2025-02-08 19:40:17 +0900

    画像とテキストの更新

--- src/components/MdxRenderer.tsx
+++ src/components/MdxRenderer.tsx
@@ -1,12 +1,18 @@
-// src/components/MdxRenderer.tsx
 "use client";
 
-import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
+import dynamic from "next/dynamic";
+import type { MDXRemoteSerializeResult } from "next-mdx-remote";
+
+// MDXRemote を通常のインポートではなく、動的にインポートして SSR を無効化します。
+const MDXRemoteDynamic = dynamic(
+  () => import("next-mdx-remote").then((mod) => mod.MDXRemote),
+  { ssr: false }
+);
 
 interface MdxRendererProps {
   mdxSource: MDXRemoteSerializeResult;
 }
 
 export default function MdxRenderer({ mdxSource }: MdxRendererProps) {
-  return <MDXRemote {...mdxSource} />;
+  return <MDXRemoteDynamic {...mdxSource} />;
 }
