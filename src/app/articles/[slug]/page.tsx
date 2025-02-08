4faeb9e49ab1e51bf1b79893b76d572588edefc8
Trin1140 <taichi1140@gmail.com>
2025-02-08 23:55:18 +0900

    画像とテキストの更新

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -4,7 +4,7 @@ import path from "path";
 import matter from "gray-matter";
 import { serialize } from "next-mdx-remote/serialize";
 import { notFound } from "next/navigation";
-import ClientArticleDetail from "./ClientArticleDetail"; // クライアント側にレンダリングを任せる
+import ClientArticleDetail from "./ClientArticleDetail";
 
 export async function generateStaticParams() {
   const articlesDirectory = path.join(process.cwd(), "content", "articles");
@@ -17,17 +17,17 @@ export async function generateStaticParams() {
 export default async function ArticleDetail({ params }: { params: { slug: string } }) {
   const { slug } = params;
   const filePath = path.join(process.cwd(), "content", "articles", `${slug}.mdx`);
-  
+
   let fileContent;
   try {
     fileContent = await fs.readFile(filePath, "utf8");
   } catch {
     notFound();
   }
-  
+
   const { data, content } = matter(fileContent);
   const mdxSource = await serialize(content);
-  
+
   return (
     <ClientArticleDetail
       title={data.title as string}
