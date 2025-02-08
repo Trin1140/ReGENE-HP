17852a4c31135d8a0a23a50b5f403ee6f6815737
Trin1140 <taichi1140@gmail.com>
2025-02-09 00:29:13 +0900

    画像とテキストの更新

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
-// next.config.js
-const withMDX = require('@next/mdx')({
-    extension: /\.mdx?$/,
-  });
-  
-  module.exports = withMDX({
-    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  });
-  
+require('dotenv').config();
+
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
+    FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
+    FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
+    SECRET_TOKEN: process.env.SECRET_TOKEN,
+  }
+};
